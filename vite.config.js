86baaa328239da38a0ee16054be583946932ffcb
Nickitas <nickitadatsky@gmail.com>
2022-08-21 15:12:33 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/nickitas/',
+  plugins: [react()]
+})
+
+// /Nickitas/nickitas.github.io.git/
+// https://github.com/Nickitas/Nickitas.github.io.git
